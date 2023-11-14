4caa518e2780ffe8dca74b10d6ac979e1041a020
Frann04 <francocazala4@gmail.com>
2023-11-14 00:01:48 -0300

    un monton de cositas

--- app/models/perro.rb
+++ app/models/perro.rb
@@ -2,4 +2,10 @@ class Perro < ApplicationRecord
   belongs_to :user
   has_many :historial_vacunas
   has_many :historial_turnos
+
+  attr_accessor :dueño_dni
+  validates :nombre, presence: { message: "No puede estar en blanco" }
+  
+
+
 end
