e2f90e0ae0c1417dfa70054cbe018920afd5d6a4
GiacoiaPedro <giacoiapedro@gmail.com>
2023-11-21 22:44:26 -0300

    todo ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2023_11_21_231541) do
-
   create_table "campaigns", force: :cascade do |t|
     t.string "nombre"
     t.string "descripcion"
